0960022b8d5578dd0cbe4773e6b1a7ed39848f00
CalebPaz19 <johan.paz@unah.hn>
2025-11-26 23:08:24 -0600

    bacend arreglado

--- ProyectoPooBackEnd/src/index.ts
+++ ProyectoPooBackEnd/src/index.ts
@@ -4,6 +4,8 @@ import { connectarBD } from "./controllers/baseDatos.controller";
 import usuariosRouter from "./routers/usuarios.router";
 import proyectosRouter from "./routers/proyectos.router";
 import codigosRouters from "./routers/codigos.routers";
+import planesRouters from "./routers/planes.router";
+import { crearPlanes } from "./controllers/planes.controller";
 
 const app: Express = express();
 
@@ -14,12 +16,14 @@ const uri  = "mongodb://localhost:27017/quickcodemaker";
 
 (async () => {
   await connectarBD(uri);
+  await crearPlanes();
 })();
 
 //localhost:8000/
 app.use("/codeMaker", usuariosRouter);
 app.use("/codeMaker", proyectosRouter);
 app.use("/codeMaker", codigosRouters);
+app.use("/codeMaker", planesRouters);
 
 app.listen(8000, () => {
     console.log("El servidor esta corriendo en el puerto 8000")
@@ -27,7 +31,4 @@ app.listen(8000, () => {
 
 
 
-function crearPlanes() {
-  throw new Error("Function not implemented.");
-}
 
